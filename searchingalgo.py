b2e4e924d74a9be0741691417030902210a7fd82
manish arya <montiarya555@gmail.com>
2018-07-31 11:04:16 +0530

    Binnary searching algorithm implemented is done

--- searchingalgo.py
+++ searchingalgo.py
@@ -1,12 +1,30 @@
 #===========================Linear search algorithm=============================
-def search(a, item):
+#def search(a, item):
+#    found = False
+#    i = 0
+#    for ele in a :
+#        if ele == item:
+#            found = True
+#        else:
+#            i = i+1
+#    return found
+#a = [10,20,30,40,50,"ram",42.3,64]
+#print(search(a,10))
+
+#=========================Binary Search algorithm===============================
+def binarySearch(mylist,item):
+    first = 0
+    last = len(mylist)-1
     found = False
-    i = 0
-    for ele in a :
-        if ele == item:
+    while first <= last and found == False:
+        mid = (first+last)/2
+        if mylist[mid] == item:
             found = True
         else:
-            i = i+1
+            if item < mylist[mid]:
+                last = mid - 1
+            else:
+                first = mid +1
     return found
-a = [10,20,30,40,50,"ram",42.3,64]
-print(search(a,10))
+mylist = [10,20,30,40,50,60,70,80,90]
+print(binarySearch(mylist,100))
