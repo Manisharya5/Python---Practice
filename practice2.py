455f5a2235048bab9c77ace678b789c8daa50da4
manish arya <montiarya555@gmail.com>
2018-08-02 10:52:56 +0530

    code for conversion of temperature is done

---
+++ practice2.py
@@ -0,0 +1,24 @@
+#convert = lambda x : (1.8)*x+32
+#a = int(input("enter a number :\n"))
+#print (convert(a))
+
+#=======================Conversion celcius to fahrenheit========================
+
+#celcius = [45.0,39.2,37.3,37.8]
+#faren = []
+#for temp in celcius:
+#    f = (9/5)*temp+32
+#    faren.append(f)
+#print(faren)
+
+#output
+#[77.0, 71.2, 69.3, 69.8]
+
+#=========================================
+
+#celcius = [45.0,39.2,37.3,37.8]
+#faren = map(lambda x: (9/5)*x+32,celcius)
+#for ele in faren:
+#    print (ele)
+
+#===============================================================================
